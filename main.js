8e7b61241912e14010aee213421cebef9d13f4c2
Maxicolella <Maxirc85@hotmail.com>
2023-02-20 23:23:47 -0300

    final

--- main.js
+++ main.js
@@ -30,12 +30,43 @@ funtion nombre (parametro1,parametro2,..., parametro n){
 
 // sumar (3,4)
 
+// ---
+
+const a = 20 
+
+alert  ('Bienvenido a Calculadora Maxi.')
+
+
+function pedirNumeros () {
+    let numero1 = parseInt(prompt("Ingrese el primer numero"));
+    let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+
+    if (isNaN(numero1) || isNaN (numero2)) {
+    alert ('error, no son números. Ingrese los números nuevamente')
+        pedirNumeros();
+    } else {
+            sumar(numero1,numero2)
+    }
+}
+
+//pedirNumeros ()
+
 function sumar (numero1, numero2){
     let mensaje= `El resultado de la operacion es ${numero1 + numero2}`;
     alert (mensaje);
 };
 
-let numero1 = parseInt(prompt("Ingrese el primer numero"));
-let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+// let numero1 = parseInt(prompt("Ingrese el primer numero"));
+// let numero2 = parseInt(prompt("Ingrese el segundo numero"));
+
+pedirNumeros   ()
 
 sumar (numero1, numero2);
+
+
+
+// if (pedirNumeros() % 2 ==  0) {
+//       alert ("El numero es par");
+//  } else {
+//       alert ("EL numero es impar");
+// }

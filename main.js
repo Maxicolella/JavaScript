0a1b8d1cc4fb9e7769a5d11c1ce8f34957ef532c
Maxicolella <Maxirc85@hotmail.com>
2023-02-22 23:06:54 -0300

    corrigeError1

--- main.js
+++ main.js
@@ -1,41 +1,4 @@
-// function saludar () {
-//     console.log ("hola")
-// }
-
-// saludar()
-
-// function pedirDatos () {
-//     let nombre = prompt ("Ingrese su Nombre");
-//     let mensaje = `Hola ${nombre}`;
-//     alert(mensaje);
-// }
-
-// pedirDatos();
-// pedirDatos();
-// pedirDatos();
-
-/*
-Estructura de una funcion con parametros
-
-funtion nombre (parametro1,parametro2,..., parametro n){
-    codigo de la funcion
-}
-*/
-
-// function sumar (numero1, numero2){
-//     let numero1 = 3
-//     let numero2 = 4
-//     console.log(numero1 + numero2);
-// };
-
-// sumar (3,4)
-
-// ---
-
-const a = 20 
-
 alert  ('Bienvenido a Calculadora Maxi.')
-
 
 function pedirNumeros () {
     let numero1 = parseInt(prompt("Ingrese el primer numero"));
@@ -49,24 +12,29 @@ function pedirNumeros () {
     }
 }
 
-//pedirNumeros ()
-
 function sumar (numero1, numero2){
     let mensaje= `El resultado de la operacion es ${numero1 + numero2}`;
     alert (mensaje);
 };
 
-// let numero1 = parseInt(prompt("Ingrese el primer numero"));
-// let numero2 = parseInt(prompt("Ingrese el segundo numero"));
-
 pedirNumeros   ()
 
-sumar (numero1, numero2);
+let operacion = prompt("Ingrese la operación");
 
+while (operacion != "ESC") {
+  switch (operacion) {
+    case "SUMAR":
+      pedirNumeros   ()
+      break;
 
+    case "RESTAR":
+      alert("SITIO EN CONSTRUCCION");
+      break;
 
-// if (pedirNumeros() % 2 ==  0) {
-//       alert ("El numero es par");
-//  } else {
-//       alert ("EL numero es impar");
-// }
+    default:
+      alert("OPERACION NO TIPIFICADA");
+      break;
+  }
+
+  operacion = prompt("Ingrese la operación");
+}
